d106ef40032be61b317a07497918c0be345bc4e3
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-16 18:41:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(a_number)
     numbers_array = (2..a_number).to_a
     numbers_array_factors = []
     numbers_array.each do |number|
-      if a_number % number == 0
+      if a_number % number == 0 && a_number != number
         numbers_array_factors << number
       end
     end
