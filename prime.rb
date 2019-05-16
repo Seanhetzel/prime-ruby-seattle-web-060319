8bf29f699455cf071a57519bb19dbc3a7f6fc8a6
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-16 18:40:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,10 @@ def prime?(a_number)
   if a_number >= 2 
     numbers_array = (2..a_number).to_a
     numbers_array_factors = []
-    puts numbers_array
     numbers_array.each do |number|
       if a_number % number == 0
         numbers_array_factors << number
       end
-      puts number
     end
   else
     false
