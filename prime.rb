d12792d949a39e7775acc81ed55e111bf4ff8467
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-16 18:31:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,11 @@
 # Add  code here!
 def prime?(a_number)
   if a_number >= 2 
-    numbers_array = (2..a_number/2).to_a
+    numbers_array = (2..a_number).to_a
     numbers_array_multiples = []
-    i = 2
-    until i > a_number/2
-      numbers_array.each do |number|
-        numbers_array_multiples << number * i
-      end
-      i += 1
-    end
-    numbers_array_multiples = numbers_array_multiples.uniq
-    if numbers_array_multiples.include?(a_number)
-      false
-    else
-      true
-    end
+    numbers_array.each do |number|
+
+
   else
     false
   end
